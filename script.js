071eeffd0a29189c359c3f75a278b31eafc58d60
Mukul Badge <badgemukul36@gmail.com>
2025-09-10 21:38:38 +0530

    display swtting

--- script.js
+++ script.js
@@ -8,23 +8,41 @@ function mod(string) {
   return (eval(string) / m);
 }
 
+
+function adjustFontSize() {
+    const maxFontSize = 40;
+    const minFontSize = 16;
+    input.style.fontSize = maxFontSize + 'px';
+    while (input.scrollWidth > input.clientWidth && parseInt(input.style.fontSize) > minFontSize) {
+        input.style.fontSize = (parseInt(input.style.fontSize) - 1) + 'px';
+    }
+}
+
+
+
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
         if (e.target.innerHTML == '=') {
             string = eval(string);
             input.value = string;
+            adjustFontSize();
         } else if (e.target.innerHTML == 'AC') {
             string = "";
             input.value = string;
+            adjustFontSize();
         } else if (e.target.innerHTML == 'DEL') {
             string = string.slice(0, -1);
             input.value = string;
+            adjustFontSize();
         } else if (e.target.innerHTML == '%') {
             string = mod(string);
             input.value = string;
+            adjustFontSize();
         } else{
             string += e.target.innerHTML;
             input.value = string;
+            input.scrollLeft = input.scrollWidth;
+            adjustFontSize();
         }
         
     });

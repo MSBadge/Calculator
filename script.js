06d8ed684880e313798f5089e97b282f96c6a359
Mukul Badge <badgemukul36@gmail.com>
2025-09-10 21:04:04 +0530

    update % operator

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let buttons = document.querySelectorAll('button');
 let string = "";
 let arr = Array.from(buttons);
 
+function mod(string) {
+    m = 100
+  return (eval(string) / m);
+}
+
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
         if (e.target.innerHTML == '=') {
@@ -15,7 +20,7 @@ arr.forEach(button => {
             string = string.slice(0, -1);
             input.value = string;
         } else if (e.target.innerHTML == '%') {
-            string = string / 100;
+            string = mod(string);
             input.value = string;
         } else{
             string += e.target.innerHTML;
